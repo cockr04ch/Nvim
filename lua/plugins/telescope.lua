c1ce57d628775677ce43e67801ed3e06794caee1
Daniel Nuñez <124007027+cockr04ch@users.noreply.github.com>
2025-07-29 00:05:17 -0400

    solve problem

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,20 +5,20 @@ return {
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { '<leader>?', require('telescope.builtin').oldfiles, desc = '[?] Find recently opened files' },
-      { '<leader><space>', require('telescope.builtin').buffers, desc = '[ ] Find existing buffers' },
+      { '<leader>?', function() require('telescope.builtin').oldfiles() end, desc = '[?] Find recently opened files' },
+      { '<leader><space>', function() require('telescope.builtin').buffers() end, desc = '[ ] Find existing buffers' },
       { '<leader>/', function()
         require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
         })
       end, desc = '[/] Fuzzily search in current buffer' },
-      { '<leader>gf', require('telescope.builtin').git_files, desc = 'Search [G]it [F]iles' },
-      { '<leader>sf', require('telescope.builtin').find_files, desc = '[S]earch [F]iles' },
-      { '<leader>sh', require('telescope.builtin').help_tags, desc = '[S]earch [H]elp' },
-      { '<leader>sw', require('telescope.builtin').grep_string, desc = '[S]earch current [W]ord' },
-      { '<leader>sg', require('telescope.builtin').live_grep, desc = '[S]earch by [G]rep' },
-      { '<leader>sd', require('telescope.builtin').diagnostics, desc = '[S]earch [D]iagnostics' },
+      { '<leader>gf', function() require('telescope.builtin').git_files() end, desc = 'Search [G]it [F]iles' },
+      { '<leader>sf', function() require('telescope.builtin').find_files() end, desc = '[S]earch [F]iles' },
+      { '<leader>sh', function() require('telescope.builtin').help_tags() end, desc = '[S]earch [H]elp' },
+      { '<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord' },
+      { '<leader>sg', function() require('telescope.builtin').live_grep() end, desc = '[S]earch by [G]rep' },
+      { '<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = '[S]earch [D]iagnostics' },
     },
     config = function()
       require("telescope").setup({
